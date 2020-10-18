49bd596a6e040013514e1e0af5181cf4de6f678f
sumitt1080 <sumitt1080@gmail.com>
2020-10-18 22:06:34 +0530

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:io';
 import 'package:geolocator/geolocator.dart';
 import 'package:intl/intl.dart';
+import 'package:simple_animations/simple_animations.dart';
+import './Animations/FadeAnimation.dart';
 
 void main() => runApp(WeatherApp());
 
@@ -131,116 +134,120 @@ class _WeatherAppState extends State<WeatherApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/$weather.png'),
-              fit: BoxFit.cover,
-              colorFilter: new ColorFilter.mode(
-                  Colors.black.withOpacity(0.6), BlendMode.dstATop),
+    return FadeAnimation(1.0,
+          MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/$weather.png'),
+                fit: BoxFit.cover,
+                colorFilter: new ColorFilter.mode(
+                    Colors.black.withOpacity(0.6), BlendMode.dstATop),
+              ),
             ),
-          ),
-          child: temperature == null
-              ? Center(child: CircularProgressIndicator())
-              : Scaffold(
-                  appBar: AppBar(
-                    actions: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(right: 20.0),
-                        child: GestureDetector(
-                          onTap: () {
-                            _getCurrentLocation();
-                          },
-                          child: Icon(Icons.location_city, size: 36.0),
-                        ),
-                      )
-                    ],
+            child: temperature == null
+                ? Center(child: CircularProgressIndicator())
+                : Scaffold(
+                    appBar: AppBar(
+                      actions: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(right: 20.0),
+                          child: GestureDetector(
+                            onTap: () {
+                              _getCurrentLocation();
+                            },
+                            child: Icon(Icons.location_city, size: 36.0),
+                          ),
+                        )
+                      ],
+                      backgroundColor: Colors.transparent,
+                      elevation: 0.0,
+                    ),
+                    resizeToAvoidBottomInset: false,
                     backgroundColor: Colors.transparent,
-                    elevation: 0.0,
-                  ),
-                  resizeToAvoidBottomInset: false,
-                  backgroundColor: Colors.transparent,
-                  body: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Column(
+                    body: FadeAnimation(1.2,
+                                          Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: <Widget>[
-                          Center(
-                            child: Image.network(
-                              'https://www.metaweather.com/static/img/weather/png/' +
-                                  abbreviation +
-                                  '.png',
-                              width: 100,
-                            ),
-                          ),
-                          Center(
-                            child: Text(
-                              temperature.toString() + ' °C',
-                              style: TextStyle(
-                                  color: Colors.white, fontSize: 60.0),
-                            ),
-                          ),
-                          Center(
-                            child: Text(
-                              location,
-                              style: TextStyle(
-                                  color: Colors.white, fontSize: 40.0),
-                            ),
-                          ),
-                        ],
-                      ),
-                      SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        
-                        child: Row(
-                          children: <Widget>[
-                            
-                            for (var i = 0; i < 7; i++)
-                              forecastElement(
-                                  i + 1,
-                                  abbreviationForecast[i],
-                                  minTemperatureForecast[i],
-                                  maxTemperatureForecast[i]),
-                          ],
-                        ),
-                      ),
-                      Column(
-                        children: <Widget>[
-                          Container(
-                            width: 300,
-                            child: TextField(
-                              onSubmitted: (String input) {
-                                onTextFieldSubmitted(input);
-                              },
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 25),
-                              decoration: InputDecoration(
-                                hintText: 'Search another location...',
-                                hintStyle: TextStyle(
-                                    color: Colors.white, fontSize: 18.0),
-                                prefixIcon:
-                                    Icon(Icons.search, color: Colors.white),
+                          Column(
+                            children: <Widget>[
+                              Center(
+                                child: Image.network(
+                                  'https://www.metaweather.com/static/img/weather/png/' +
+                                      abbreviation +
+                                      '.png',
+                                  width: 100,
+                                ),
                               ),
+                              Center(
+                                child: Text(
+                                  temperature.toString() + ' °C',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 60.0),
+                                ),
+                              ),
+                              Center(
+                                child: Text(
+                                  location,
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 40.0),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            
+                            child: Row(
+                              children: <Widget>[
+                                
+                                for (var i = 0; i < 7; i++)
+                                  forecastElement(
+                                      i + 1,
+                                      abbreviationForecast[i],
+                                      minTemperatureForecast[i],
+                                      maxTemperatureForecast[i]),
+                              ],
                             ),
                           ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(right: 32.0, left: 32.0),
-                            child: Text(errorMessage,
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    color: Colors.redAccent,
-                                    fontSize:
-                                        Platform.isAndroid ? 15.0 : 20.0)),
-                          )
+                          Column(
+                            children: <Widget>[
+                              Container(
+                                width: 300,
+                                child: TextField(
+                                  onSubmitted: (String input) {
+                                    onTextFieldSubmitted(input);
+                                  },
+                                  style:
+                                      TextStyle(color: Colors.white, fontSize: 25),
+                                  decoration: InputDecoration(
+                                    hintText: 'Search another location...',
+                                    hintStyle: TextStyle(
+                                        color: Colors.white, fontSize: 18.0),
+                                    prefixIcon:
+                                        Icon(Icons.search, color: Colors.white),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(right: 32.0, left: 32.0),
+                                child: Text(errorMessage,
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color: Colors.redAccent,
+                                        fontSize:
+                                            Platform.isAndroid ? 15.0 : 20.0)),
+                              )
+                            ],
+                          ),
                         ],
                       ),
-                    ],
-                  ),
-                )),
+                    ),
+                  )),
+      ),
     );
   }
 }
